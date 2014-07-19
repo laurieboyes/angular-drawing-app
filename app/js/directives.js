1624e7db0fc3606513ecfc0d9e9a811cd6fe0056
Lrnk <laurie.boyes@gmail.com>
2014-07-20 00:11:32 +0100

    Added a Raphael canvas

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,16 @@
 
 /* Directives */
 
-
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+
+    directive('canvas', [function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elements, attrs) {
+                var paper = Raphael(elements[0], attrs.canvasWidth, attrs.canvasHeight);
+                var background = paper.rect(0, 0, attrs.canvasWidth, attrs.canvasHeight);
+                background.attr("fill", "#FFF");
+                background.attr("stroke", "#000");
+            }
+        }
+    }]);
