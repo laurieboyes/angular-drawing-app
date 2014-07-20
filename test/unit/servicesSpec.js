3961bd557d428dfd0588caa311690dc50650e289
Lrnk <laurie.boyes@gmail.com>
2014-07-20 14:09:55 +0100

    Mocks not working

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,13 +2,59 @@
 
 /* jasmine specs for services go here */
 
-describe('service', function() {
-  beforeEach(module('myApp.services'));
+
+//Pinched from here: http://eclipsesource.com/blogs/2014/03/27/mocks-in-jasmine-tests/
+window.mock = function (constr, name) {
+    var keys = [];
+    for (var key in constr.prototype) {
+        keys.push(key);
+    }
+    return keys.length > 0 ? jasmine.createSpyObj(name || "mock", keys) : {};
+};
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+describe('service', function () {
+    beforeEach(module('myApp.services'));
+
+
+    describe('tools', function () {
+        it('the first colour in the list should be the default selected', inject(function (tools) {
+//            todo perhaps remove assumption about what the first colour is?
+            expect(tools.colours[0].name).toEqual("Black");
+            expect(tools.selectedColour.name).toEqual("Black");
+        }));
+    });
+
+    describe('drawing', function () {
+
+
+//    Mocks
+        var PaperMock = {
+            path: function () {
+                return this
+            }.bind(this),
+            attr: function () {
+                return this
+            }.bind(this)
+        };
+
+        var ToolsMock = {selectedColour: {colourValue: ""} };
+
+        beforeEach(function () {
+            var tools = mock(ToolsMock);
+            module(function ($provide) {
+                $provide.value('tools', tools);
+            });
+        });
+
+        it('starting a path puts it in the right place', inject(function (drawing) {
+
+            var paper = mock(PaperMock);
+
+            drawing.startPath(paper, 1000, 10);
+
+            expect(paper.path).toHaveBeenCalledWith("M1000,10");
+        }));
+    });
+
 });
