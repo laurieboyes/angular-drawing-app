7b4d311464c39b6eb837458633ed17210fc4d8c0
Lrnk <laurie.boyes@gmail.com>
2014-07-20 10:41:15 +0100

    Can draw but can't stop

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,11 +4,50 @@
 
 angular.module('myApp.directives', [])
 
-    .directive('canvas', ['raphael', function(raphael) {
+    .directive('canvas', [function () {
         return {
             restrict: 'A',
-            link: function(scope, elements, attrs) {
-                raphael.makeCanvas(elements[0], attrs.canvasWidth, attrs.canvasHeight);
+            link: function (scope, elements, attrs) {
+                var w = attrs.canvasWidth;
+                var h = attrs.canvasHeight;
+                var canvasElement = elements[0];
+
+                var paper = Raphael(canvasElement, w, h);
+
+                //background
+                var background = paper.rect(0, 0, w, h)
+                    .attr("fill", "#FFF")
+                    .attr("stroke", "#000");
+
+
+                //example lines (temp)
+                paper.path("M10,20L30,40");
+
+
+                var pathStringInProgress = "";
+                var pathInProgress;
+
+                //events
+                background
+                    .mousedown(function (e) {
+                        // start path
+                        pathStringInProgress = "M" + e.offsetX + "," + e.offsetY;
+                        pathInProgress = paper.path(pathStringInProgress);
+                    })
+                    .mousemove(function (e) {                        
+                        // continue path
+                        if(pathStringInProgress.length){
+                            pathStringInProgress += "L" + e.offsetX + "," + e.offsetY;
+                            pathInProgress.remove();
+                            pathInProgress = paper.path(pathStringInProgress);
+                        }
+                    })
+                    .mouseup(function (e) {
+                        // end path
+                        console.log('up!');
+                        pathStringInProgress = "";
+                    });
+
             }
         }
     }]);
--- app/js/services.js
+++ app/js/services.js
@@ -3,24 +3,5 @@
 /* Services */
 
 angular.module('myApp.services', [])
-    .service('raphael', [function() {
-        return {
-            makeCanvas : function(element, w,h) {
-                Raphael(element, w, h)
-                    .rect(0, 0, w, h)
-                    .attr("fill", "#FFF")
-                    .attr("stroke", "#000")
-
-                    .mousedown(function(e) {
-                        this.canvasMouseDown(e.offsetX, e.offsetY);
-                    }.bind(this));
-            },
-
-            canvasMouseDown : function(x, y) {
-                alert('Clicked here: x=' + x + ' y=' + y);
-            }
-
-        };
-    }])
 
 ;
