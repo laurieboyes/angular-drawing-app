9c467ffd24d73c1bd84b92a7f758de99e8fff044
Lrnk <laurie.boyes@gmail.com>
2014-07-20 14:58:38 +0100

    Tests mock paper object

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,17 +2,6 @@
 
 /* jasmine specs for services go here */
 
-
-//Pinched from here: http://eclipsesource.com/blogs/2014/03/27/mocks-in-jasmine-tests/
-window.mock = function (constr, name) {
-    var keys = [];
-    for (var key in constr.prototype) {
-        keys.push(key);
-    }
-    return keys.length > 0 ? jasmine.createSpyObj(name || "mock", keys) : {};
-};
-
-
 describe('service', function () {
     beforeEach(module('myApp.services'));
 
@@ -27,33 +16,22 @@ describe('service', function () {
 
     describe('drawing', function () {
 
-
-//    Mocks
-        var PaperMock = {
-            path: function () {
-                return this
-            }.bind(this),
-            attr: function () {
-                return this
-            }.bind(this)
+//        mocks
+        var MockPaper = function() {
+            return {
+                path: function () {return this},
+                attr: function () {return this}
+            }
         };
 
-        var ToolsMock = {selectedColour: {colourValue: ""} };
-
-        beforeEach(function () {
-            var tools = mock(ToolsMock);
-            module(function ($provide) {
-                $provide.value('tools', tools);
-            });
-        });
-
         it('starting a path puts it in the right place', inject(function (drawing) {
-
-            var paper = mock(PaperMock);
+            var paper = MockPaper();
+            spyOn(paper, 'path').andCallThrough();
 
             drawing.startPath(paper, 1000, 10);
 
             expect(paper.path).toHaveBeenCalledWith("M1000,10");
+
         }));
     });
 
