447612522e9f39f3ec202b2d95fd6fb5e11b0c0c
Lrnk <laurie.boyes@gmail.com>
2014-07-20 22:12:48 +0100

    Made interface react to selected tool

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,50 +3,48 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('ColourController', ['controlsState', 'drawing', '$scope', function (controlsState, drawing, $scope) {
+    
+//    todo look into nesting controllers?
+    
+    .controller('ControlsController', ['controlsState', 'drawing', '$scope', function (controlsState, drawing, $scope) {
+
+        // Colours
         
         $scope.colours = controlsState.colours;
-
         $scope.setSelectedColour = function() {
             controlsState.selectedColour = $scope.selectedColour;
         };
-
         $scope.$watch( function () { return controlsState.selectedColour; }, function ( selectedColour ) {
             $scope.selectedColour = selectedColour;
         });
-    }])
-
-    .controller('ToolsController', ['controlsState', 'drawing', '$scope', function (controlsState, drawing, $scope) {
+        
+        
+        // Tools
         $scope.tools = controlsState.tools;
-
         $scope.setSelectedTool = function() {
             controlsState.selectedTool = $scope.selectedTool;
-            
-//            Not sure this belongs here
-            if($scope.selectedTool.id === "pen") {
+
+            if ($scope.selectedTool.id === "pen") {
                 drawing.enablePathDrawing();
             } else {
                 drawing.disablePathDrawing();
             }
         };
-
-        $scope.$watch( function () { return controlsState.selectedTool; }, function ( selectedTool ) {           
+        $scope.$watch( function () { return controlsState.selectedTool; }, function ( selectedTool ) {
             $scope.selectedTool = selectedTool;
         });
-    }])
 
-    .controller('SizeController', ['controlsState', '$scope', function (controlsState, $scope) {
+//        Pen Size
+        
         $scope.sizes = controlsState.sizes;
-
         $scope.setSelectedSize = function() {
             controlsState.selectedSize = $scope.selectedSize;
         };
-
         $scope.$watch( function () { return controlsState.selectedSize; }, function ( selectedSize ) {
             $scope.selectedSize = selectedSize;
-        });
+        })
     }])
-    
+            
     .controller('ClearController', ['drawing', '$scope', function (drawing, $scope) {
         
 //        Clear
