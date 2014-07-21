ac59a236a2ed658da8d00363c911d241516929fe
Lrnk <laurie.boyes@gmail.com>
2014-07-21 19:49:48 +0100

    Refactored tool controller code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,23 +4,30 @@
 
 angular.module('myApp.controllers', [])
     
-//    todo look into nesting controllers?
-    
-    .controller('ControlsController', ['toolState', 'drawing', '$scope', function (toolState, drawing, $scope) {
+    .controller('ToolsStateController', ['toolState', 'drawing', '$scope', function (toolState, drawing, $scope) {
 
-        // Colours
-        
-        $scope.colours = toolState.colours;
-        $scope.setSelectedColour = function() {
-            toolState.selectedColour = $scope.selectedColour;
-        };
+        // Colours        
+        $scope.colours = toolState.colours;        
         $scope.$watch( function () { return toolState.selectedColour; }, function ( selectedColour ) {
             $scope.selectedColour = selectedColour;
         });
-        
-        
+                
         // Tools
-        $scope.tools = toolState.tools;
+        $scope.tools = toolState.tools;        
+        $scope.$watch( function () { return toolState.selectedTool; }, function ( selectedTool ) {
+            $scope.selectedTool = selectedTool;
+        });
+
+//        Pen Size
+        
+        $scope.sizes = toolState.sizes;
+        $scope.$watch( function () { return toolState.selectedSize; }, function ( selectedSize ) {
+            $scope.selectedSize = selectedSize;
+        })
+    }])
+
+    .controller('ToolController', ['toolState', 'drawing', '$scope', function (toolState, drawing, $scope) {
+
         $scope.setSelectedTool = function() {
             toolState.selectedTool = $scope.selectedTool;
 
@@ -30,26 +37,31 @@ angular.module('myApp.controllers', [])
                 drawing.disablePathDrawing();
             }
         };
-        $scope.$watch( function () { return toolState.selectedTool; }, function ( selectedTool ) {
-            $scope.selectedTool = selectedTool;
-        });
 
-//        Pen Size
-        
-        $scope.sizes = toolState.sizes;
+    }])
+
+    .controller('ColourController', ['toolState', '$scope', function (toolState, $scope) {
+
+        $scope.setSelectedColour = function() {
+            toolState.selectedColour = $scope.selectedColour;
+        };
+
+    }])
+
+    .controller('SizeController', ['toolState', '$scope', function (toolState, $scope) {
+
         $scope.setSelectedSize = function() {
             toolState.selectedSize = $scope.selectedSize;
         };
-        $scope.$watch( function () { return toolState.selectedSize; }, function ( selectedSize ) {
-            $scope.selectedSize = selectedSize;
-        })
-    }])
-            
-    .controller('ClearController', ['drawing', '$scope', function (drawing, $scope) {
         
-//        Clear
+    }])
+
+
+            
+    .controller('ClearController', ['drawing', '$scope', function (drawing, $scope) {        
+        
         $scope.clear = function() {
             drawing.clear();
         }
-
+        
     }]);
